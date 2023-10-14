6e1547f03755d6f92b3d30dc53a38cb6e1be2208
liao <694117837@qq.com>
2023-10-14 21:47:06 +0800

    用户信息设计

--- qingyue_app/pages/user/user.js
+++ qingyue_app/pages/user/user.js
@@ -1,4 +1,12 @@
 Page({
-  data: {},
+  data: {
+    usermes:{
+      username:"username",
+      td_kil:"xxx",         //今日公里数
+      trip_num:"xxx",       //出行次数
+      total_kil:"xxx",      //总公里数
+
+    }
+  },
   onLoad() {},
 });
