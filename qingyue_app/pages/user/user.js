c3fb25cf73af66570aa4c8c14711fa89d699ece7
liao <694117837@qq.com>
2023-10-15 10:54:12 +0800

    用户界面完善

--- qingyue_app/pages/user/user.js
+++ qingyue_app/pages/user/user.js
@@ -5,8 +5,13 @@ Page({
       td_kil:"xxx",         //今日公里数
       trip_num:"xxx",       //出行次数
       total_kil:"xxx",      //总公里数
-
-    }
+    },
+    clock_in: true,
+  },
+  handle_clock(){
+    this.setData({
+      clock_in: false,
+    });
   },
   onLoad() {},
 });
