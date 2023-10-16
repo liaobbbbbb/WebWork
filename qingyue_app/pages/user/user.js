c84221f5741d2856652fb317728bb0a8a27c80d8
liao <694117837@qq.com>
2023-10-16 14:17:41 +0800

    打卡功能完善

--- qingyue_app/pages/user/user.js
+++ qingyue_app/pages/user/user.js
@@ -6,12 +6,21 @@ Page({
       trip_num:"xxx",       //出行次数
       total_kil:"xxx",      //总公里数
     },
-    clock_in: true,
+    show_clock_in: false,
+    clock_in: false,
   },
   handle_clock(){
+    ci1 = this.data.clock_in
     this.setData({
-      clock_in: false,
+      clock_in: true,
     });
+    ci2 = this.data.clock_in
+    // 向数据库中传入打卡成功信息
+    if(!ci1&&ci2){
+      this.setData({
+        show_clock_in: true
+      })
+    }
   },
   show_id_page(){
     my.navigateTo({
