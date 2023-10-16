02c1748124437532166aca4cc9b2ff6a44c89e07
liao <694117837@qq.com>
2023-10-16 13:56:22 +0800

    账号设置页面

--- qingyue_app/pages/user/user_page/user_page.js
+++ qingyue_app/pages/user/user_page/user_page.js
@@ -8,7 +8,12 @@ Page({
       user_alipay:'*宝斌',
     },
     show_change_head: false,
-
+    show_change_name: false,
+    show_change_sex: false,
+    show_change_num: false,
+    change_name_foucs: true,
+    new_name:"",
+    change_name_bar_active:''
   },
   navigate_to_user(){
     my.navigateTo({
@@ -20,5 +25,58 @@ Page({
       show_change_head: !this.data.show_change_head
     });
   },
+  change_sex(){
+    this.setData({
+      show_change_sex: !this.data.show_change_sex
+    });
+  },
+  change_name(e){
+    this.setData({
+      show_change_name: !this.data.show_change_name
+    })
+  },
+  change_name_input_on(){
+    this.setData({
+      change_name_bar_active: 'change_name_bar_active'
+    })
+  },
+  change_name_input_off(){
+    this.setData({
+      change_name_bar_active: ''
+    })
+  },
+  change_name_bind(e){
+    this.setData({
+      new_name: e.detail.value
+    })
+  },
+  handle_change_name(){
+    console.log(this.data.new_name)
+    // 对新名字进行合规检查
+
+    // 将新的名字改入数据库
+  },
+  chnage_sex_male(){
+    this.setData({
+      'user_mes.user_sex': '男',
+       show_change_sex: !this.data.show_change_sex
+    })
+    // 修改数据库
+  },
+  change_sex_female(){
+    this.setData({
+      'user_mes.user_sex': '女',
+      show_change_sex: !this.data.show_change_sex
+    })
+    // 修改数据库
+  },
+  change_num(){
+    this.setData({
+      show_change_num: !this.data.show_change_num
+    })
+  },
   onLoad() {},
+  onKeyboardHeight(){
+    // 键盘高度变化时触发
+  }
 });
